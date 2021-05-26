f014f081663aa0dcd2349e7d074cd9ec0cfc6866
christopher-christofi <christopherlchristofi@outlook.com>
2021-05-26 19:10:56 +0100

    Update db tabular structure to simple, fitting raw

--- data/database/raw/configuration.py
+++ data/database/raw/configuration.py
@@ -1,8 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from data.database.raw.resources import raw_database_naming
+from data.resources import raw_database_naming
 
 raw_database_path = "sqlite:///data/database/raw/{database}".format(
     database=raw_database_naming
@@ -10,30 +9,18 @@ raw_database_path = "sqlite:///data/database/raw/{database}".format(
 
 Base = declarative_base()
 
-class TwitterUser(Base):
-    __tablename__ = 'twitteruser'
-
-    id = Column(Integer, primary_key=True)
-    user = Column(Integer, nullable=False)
-
-class TwitterTweet(Base):
-    __tablename__ = 'twittertweet'
-
-    id = Column(Integer, primary_key=True)
-    tweet_id = Column(Integer, nullable=False)
-    tweet_text = Column(String(250))
-    user_id = Column(Integer, ForeignKey('twitteruser.user'))
-    twitteruser = relationship(TwitterUser)
-
-class TweetDate(Base):
-    __tablename__ = 'tweetdate'
+class RawTwitterTweet(Base):
+    __tablename__= 'rawtwittertweet'
 
     id = Column(Integer, primary_key=True)
     tweet_date = Column(String(250), nullable=False)
-    tweet_id = Column(Integer, ForeignKey('twittertweet.tweet_id'))
-    twittertweet = relationship(TwitterTweet)
+    user = Column(Integer, nullable=False)
+    tweet_id = Column(Integer, nullable=False)
+    tweet_text = Column(String(250))
+    tweet_lang = Column(String(250))
 
 def raw_build(run=0):
+    """Function that initiates the build order of the database for raw datasets."""
 
     if run == True:
         engine = create_engine(raw_database_path)
